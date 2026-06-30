ff77457e5e6f45baf76b2192c6e7b0c3585e7f37
denis <dsushkov@k-tech.ru>
2026-06-30 19:44:34 +0300

    Simplification of images

--- services/main-api/entrypoint.sh
+++ services/main-api/entrypoint.sh
@@ -2,6 +2,6 @@
 # Перед стартом приложения накатываем миграции на БД, затем запускаем uvicorn.
 set -e
 
-uv run alembic upgrade head
+alembic upgrade head
 
-exec uv run uvicorn app.main:app --host 0.0.0.0 --port 8000
+exec uvicorn app.main:app --host 0.0.0.0 --port 8000
